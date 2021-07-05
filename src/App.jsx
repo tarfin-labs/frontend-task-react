41555566f6c28ade36b87d0356e126b79363b86f
Erhan Namal <namallerhan@gmail.com>
2021-07-05 16:01:35 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <img src="https://tarfin.com/img/logo.svg" alt="Tarfin Logo" />
+    </div>
+  )
+}
+
+export default App
